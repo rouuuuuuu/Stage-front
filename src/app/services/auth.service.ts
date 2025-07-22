87aa646e284fe929e0dc40b5963f29923a61553c
rouuuuuuu <rrrooouuuaaa401@gmail.com>
2025-07-22 23:13:30 +0100

    commit

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
 
 @Injectable({ providedIn: 'root' })
 export class AuthService {
@@ -9,6 +10,12 @@ export class AuthService {
   constructor(private http: HttpClient) {}
 
   login(credentials: { username: string; password: string }): Observable<any> {
-    return this.http.post(`${this.apiUrl}/login`, credentials);
-  }
+  return this.http.post<{ token: string }>(`${this.apiUrl}/login`, credentials).pipe(
+    tap(response => {
+      if (response.token) {
+        localStorage.setItem('token', response.token);  // save token for later
+      }
+    })
+  );
+}
 }
